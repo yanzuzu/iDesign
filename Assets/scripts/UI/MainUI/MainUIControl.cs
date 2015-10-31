1185ca53c3bf0d904bb803dd0a8e75c5a4a247ca
Sam Lo <sam@randomidea.com>
2015-10-31 12:15:03 +0800

    add MainUI and tabControl

---
+++ Assets/scripts/UI/MainUI/MainUIControl.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainUIControl : MonoBehaviour {
+	[SerializeField]
+	private TabUIControl m_tabUIControl;
+
+	void Start()
+	{
+		m_tabUIControl.SetTabActiveIdx (0);
+	}
+}
---
+++ Assets/scripts/UI/common/TabUIControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TabUIControl : MonoBehaviour {
+	[SerializeField]
+	private List<UIButton> m_tabBtns = new List<UIButton>();
+	
+	void Awake()
+	{
+		for( int i = 0 ; i < m_tabBtns.Count ; i ++ )
+		{
+			int pIdx = i;
+			EventDelegate.Set(m_tabBtns[i].onClick, delegate {
+				onClickTabBtn(pIdx);	
+			});
+		}
+	}
+
+	public void SetTabActiveIdx( int pIdx )
+	{
+		for( int i = 0 ; i < m_tabBtns.Count ; i ++ )
+		{
+			m_tabBtns[i].isEnabled = ( i != pIdx );
+		}
+	}
+
+	private void onClickTabBtn(int userData)
+	{
+		ZuDebug.Log ("OnClicktTabBtn userData = " +  userData);
+		SetTabActiveIdx (userData);
+	}
+}
