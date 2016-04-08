5ded8ef65e0cdc22396aa3ef543ebba411b459d8
Sam Lo <sam@randomidea.com>
2016-04-08 22:31:40 +0800

    remove unused script and assets. Restart our project!!

--- Assets/scripts/Event/EventIDs.cs
+++ Assets/scripts/Event/EventIDs.cs
@@ -2,9 +2,5 @@
 using System.Collections;
 
 public class EventIDs{
-
-	static public string EVENT_ON_CLICK_PARENT_NODE = "EVENT_ON_CLICK_PARENT_NODE"; 
-	static public string EVENT_ON_CLICK_CHILD_NODE = "EVENT_ON_CLICK_CHILD_NODE";
-
-	static public string EVENT_ON_CLICK_UNIT_TYPE = "EVENT_ON_CLICK_UNIT_TYPE";
+	
 }
